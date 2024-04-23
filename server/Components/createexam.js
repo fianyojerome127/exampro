0fba89d57c938aa758707bc5ad8ea08088ab39b5
fianyojerome127 <89052365+fianyojerome127@users.noreply.github.com>
2024-04-23 04:37:14 +0000

    uuid

--- server/Components/createexam.js
+++ server/Components/createexam.js
@@ -1,3 +1,5 @@
+import { v4 as uuidv4 } from 'uuid';
+
 // Load examination details from localStorage
 function loadExaminationDetails() {
     const details = JSON.parse(localStorage.getItem('examinationDetails'));
